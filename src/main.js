39416edfb55998133efe6ec17c0e9590e646dc40
Gokuldev23 <gokularise@gmail.com>
2024-04-24 14:13:51 +0530

    sw added

--- src/main.js
+++ src/main.js
@@ -1,6 +1,20 @@
 import './app.css'
 import App from './App.svelte'
 
+// main.js or app.js
+
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', function() {
+    navigator.serviceWorker.register('/service-worker.js')
+      .then(function(registration) {
+        console.log('Service Worker registered with scope:', registration.scope);
+      })
+      .catch(function(error) {
+        console.error('Service Worker registration failed:', error);
+      });
+  });
+}
+
 const app = new App({
   target: document.getElementById('app'),
 })
