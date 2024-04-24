ce779b10ca65e10963a6cfca6397f14e6720f2d7
Gokuldev23 <gokularise@gmail.com>
2024-04-24 14:33:13 +0530

    sw added

--- src/App.svelte
+++ src/App.svelte
@@ -4,20 +4,20 @@
   import Counter from "./lib/Counter.svelte";
 
   // Check if the browser supports service workers
-  if ("serviceWorker" in navigator) {
-    // Register the service worker
-    navigator.serviceWorker
-      .register("../service-worker.js")
-      .then(function (registration) {
-        console.log(
-          "Service Worker registered with scope:",
-          registration.scope
-        );
-      })
-      .catch(function (error) {
-        console.error("Service Worker registration failed:", error);
-      });
-  }
+  // if ("serviceWorker" in navigator) {
+  //   // Register the service worker
+  //   navigator.serviceWorker
+  //     .register("../service-worker.js")
+  //     .then(function (registration) {
+  //       console.log(
+  //         "Service Worker registered with scope:",
+  //         registration.scope
+  //       );
+  //     })
+  //     .catch(function (error) {
+  //       console.error("Service Worker registration failed:", error);
+  //     });
+  // }
 
   // Function to display a notification
   function showNotification() {
--- src/main.js
+++ src/main.js
@@ -3,6 +3,18 @@ import App from './App.svelte'
 
 // main.js or app.js
 
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', function() {
+    navigator.serviceWorker.register('../service-worker.js')
+      .then(function(registration) {
+        console.log('Service Worker registered with scope:', registration.scope);
+      })
+      .catch(function(error) {
+        console.error('Service Worker registration failed in  main:', error);
+      });
+  });
+}
+
 const app = new App({
   target: document.getElementById('app'),
 })
